ee09b19622d103c02b63704008590d5a610aea5f
Hugo de Moraes <hugoc.bmoraes@gmail.com>
2017-03-30 00:15:21 -0300

    Rollback

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import express from 'express'
+var express = require('express');
 var mongoose = require('mongoose');
 var port = process.env.PORT || 8080;
 var database = require('./config/database');
